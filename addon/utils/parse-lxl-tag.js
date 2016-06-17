7a432827318f704bfc83ff1efac5653fb5a23fa9
null null null <null@ember.theater>
2016-06-17 07:31:47 -0700

    get working params

--- addon/utils/parse-lxl-tag.js
+++ addon/utils/parse-lxl-tag.js
@@ -1,6 +1,21 @@
+function matchParamsAndHash(string) {
+  let m;
+  const re = /"([^"]*)"|'([^']*)'|[^\s"']+/g;
+  const result = [];
+
+  while ((m = re.exec(string)) !== null) {
+    if (m.index === re.lastIndex) {
+      re.lastIndex++;
+    }
+    result.push(m[1] || m[2] || m[0]);
+  }
+
+  return result;
+}
+
 export default function parseLxlTag(text) {
   const [, openingOrClosing, content] = text.match(/\(\((#|\/)?(.*?)\)\)/);
-  const params = content.match(/[^\s"']+|"([^"]*)"|'([^']*)'/g);
+  const params = matchParamsAndHash(content);
   const tagName = params.shift();
   const method = openingOrClosing === '/' ? 'stop' : 'start';
   const isOpening = openingOrClosing === '#';
