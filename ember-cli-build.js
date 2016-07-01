4d87d68c242199ec59557f70adc38ee56ae9201d
null null null <null@revolution.cheap>
2016-07-01 14:02:36 -0700

    includePolyfill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,10 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
-    stringifyPath: 'markdown'
+    stringifyPath: 'markdown',
+    babel: {
+      includePolyfill: true
+    }
   });
 
   app.import("bower_components/highlightjs/styles/monokai-sublime.css");
