c835bb1e00abd48ccaf931a5fbb2eff5c6295dc7
null null null <null@revolution.cheap>
2016-07-01 17:34:17 -0700

    make digestable

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-letter-by-letter'
+  name: 'ember-letter-by-letter',
+
+  included: function(app) {
+    this._super.included(app);
+
+    this.eachAddonInvoke('included', [app]);
+  }
 };
